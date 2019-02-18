c7ccaa3d3aa53b1bb427994b8339613b093463da
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-18 23:45:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,7 @@
 def prime?(int)
   if int <= 1 
     return false
-  elsif int == 2 
-    return true 
-  elsif int == 3 
+  elsif int == 2 || int == 3
     return true 
   elsif int > 3
     nums = (2..int-1).to_a 
